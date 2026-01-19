0bcd99094c0b5091ac5243a2797f274ef8d60e2c
Ian Rose <ianrose14@gmail.com>
2026-01-19 11:16:51 -0500

    clean up application form

--- scripts/gen_application.py
+++ scripts/gen_application.py
@@ -6,7 +6,7 @@ import pymupdf
 # * https://pymupdf.readthedocs.io/en/latest/widget.html
 
 def_fontsize = 10
-margin = 30
+margin = 20
 def_font = 'helv'
 
 def create_application_form(output_path="application.pdf"):
@@ -15,6 +15,7 @@ def create_application_form(output_path="application.pdf"):
 
     r = page.bound()
     xmax = r[2] - margin
+    ymax = r[3] - margin
     img_rect = pymupdf.Rect(margin, margin, 60, 60)
     page.insert_image(img_rect, filename="img/ar_logo_128.png")
 
@@ -23,12 +24,16 @@ def create_application_form(output_path="application.pdf"):
         h = line_height(pymupdf.Font(def_font), fontsize)
 
         text = 'Dr. Allison Thomas Rose'
-        text_length1 = pymupdf.get_text_length(text, fontname=def_font, fontsize=fontsize)
-        page.insert_text((xmax - text_length1, h + margin), text, fontname=def_font, fontsize=fontsize)
+        text_length = pymupdf.get_text_length(text, fontname=def_font, fontsize=fontsize)
+        page.insert_text((xmax - text_length, h + margin), text, fontname=def_font, fontsize=fontsize)
 
         text = 'Memorial Fund'
-        text_length2 = pymupdf.get_text_length(text, fontname=def_font, fontsize=fontsize)
-        page.insert_text((xmax - text_length2, h + margin + 13), text, fontname=def_font, fontsize=fontsize)
+        text_length = pymupdf.get_text_length(text, fontname=def_font, fontsize=fontsize)
+        page.insert_text((xmax - text_length, 2*h + margin), text, fontname=def_font, fontsize=fontsize)
+
+        text = 'A 501(c)(3) nonprofit organization'
+        text_length = pymupdf.get_text_length(text, fontname=def_font, fontsize=fontsize-2)
+        page.insert_text((xmax - text_length, 3*h + margin), text, fontname=def_font, fontsize=fontsize-2)
 
     insert_fund_name()
 
@@ -39,6 +44,8 @@ def create_application_form(output_path="application.pdf"):
         page.insert_text((x, y+h), text, fontname=fontname, fontsize=fontsize)
         if underline:
             page.draw_line((x, y+h+8), (x + text_length, y+h+8), color=(0, 0, 0), width=1)
+            return y + h + 1
+        return y + h
 
     title = 'Application Form'
     insert_centered_text(margin, title, fontsize=def_fontsize+8, underline=True)
@@ -46,8 +53,7 @@ def create_application_form(output_path="application.pdf"):
     ypos = 90
 
     lines = [
-        "Instructions:",
-        "  1. Please completely fill out this form, typing all responses.",
+        "  1. Please complete all sections of this form.  Typed responses are preferred.",
         "  2. Sign and date the form at the bottom.",
         "  3. Visit https://www.allisonrosememorialfund.org/apply.html to submit and view next steps.",
         "  4. Questions?  Please contact ianrose@allisonrosememorialfund.org"
@@ -97,37 +103,67 @@ def create_application_form(output_path="application.pdf"):
         [{"label": "NNP degree-granting program(s) you currently attend or are applying to for admission:"}],
         [{"name": "program_field", "size": -1}],
         [{"label": "Please check exactly one (1) of the following:"}],
-        [{"name": "current_fulltime_check", "label": "I am currently enrolled in the FULL-time NNP program listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
-        [{"name": "current_parttime_check", "label": "I am currently enrolled in the PART-time NNP program listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
+        [{"name": "current_fulltime_check", "label": "I am already enrolled in the FULL-time NNP program listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
+        [{"name": "current_parttime_check", "label": "I am already enrolled in the PART-time NNP program listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
         [{"name": "applying_fulltime_check", "label": "I am currently applying to the FULL-time NNP program(s) listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
-        [{"name": "applying_parttime_check", "label": "I am currently applying to the PART-time NNP programs(s) listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
+        [{"name": "applying_parttime_check", "label": "I am currently applying to the PART-time NNP program(s) listed above.", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
         [{"name": "applying_other_check", "label": "Other (please explain below):", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True}],
         [{"name": "applying_other_field1", "size": -1}],
         [{"name": "applying_other_field2", "size": -1}],
+        [],
         [
-            {"name": "start_date_field", "label": "Intended start date", "size": 100},
-            {"name": "fulltime_field", "label": "Do you plan to attend full-time?", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX},
-            {"name": "parttime_field", "label": "Or part-time?", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX},
+            {"name": "start_date_field", "label": "Intended start date", "size": 90},
+            {"label": "          Planned enrollment status:"},
+            {"name": "fulltime_field", "label": "Full-time", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True},
+            {"name": "parttime_field", "label": "Part-time", "size": 20, "type": pymupdf.PDF_WIDGET_TYPE_CHECKBOX, "reverse": True},
         ],
         [
             {"label": "If part-time, please list your anticipated course of study, including credit-hours per semester."},
         ],
         [
-            {"name": "parttime_details_year1_field", "label": "Year 1", "size": -1},
+            {"name": "parttime_details_year1_field", "size": -1},
         ],
         [
-            {"name": "parttime_details_year2_field", "label": "Year 2", "size": -1},
+            {"name": "parttime_details_year2_field", "size": -1},
         ],
         [
-            {"name": "parttime_details_year3_field", "label": "Year 3", "size": -1},
-        ],
-        [
-            {"name": "parttime_details_year4_field", "label": "Year 4", "size": 410 },
-            {"label": "(if applicable)"}
+            {"name": "parttime_details_year3_field", "size": -1},
         ]
     ]
-
     ypos = insert_form_fields(rows, page, ypos)
+
+    ypos += 20
+
+    page.draw_line((margin, ypos), (xmax, ypos), color=(.6, .6, .6), width=1)
+    ypos += 18
+
+    text = 'Applicant Certification'
+    page.insert_text((margin, ypos), text, fontname=def_font, fontsize=def_fontsize+2)
+    ypos += 20
+
+    text = 'I certify that the information provided in this application is accurate and complete to the best of my knowledge,'
+    page.insert_text((margin + 10, ypos), text, fontname=def_font, fontsize=def_fontsize)
+    ypos += 13
+    text = 'and I understand that providing false information may affect my eligibility for this scholarship.'
+    page.insert_text((margin + 10, ypos), text, fontname=def_font, fontsize=def_fontsize)
+    ypos += 35
+
+    text = 'Applicant Signature:'
+    xpos = margin + 10
+    page.insert_text((xpos, ypos), text, fontname=def_font, fontsize=def_fontsize)
+    text_length = pymupdf.get_text_length(text, fontname=def_font, fontsize=def_fontsize)
+    xpos += text_length + 5
+    page.draw_line((xpos, ypos+1), (xpos + 250, ypos+1), color=(0, 0, 0), width=1)
+    xpos += 250 + 10
+    text = 'Date:'
+    page.insert_text((xpos, ypos), text, fontname=def_font, fontsize=def_fontsize)
+    text_length = pymupdf.get_text_length(text, fontname=def_font, fontsize=def_fontsize)
+    xpos += text_length + 5
+    page.draw_line((xpos, ypos+1), (xpos + 80, ypos+1), color=(0, 0, 0), width=1)
+
+    text = 'Dr. Allison Thomas Rose Memorial Fund | allisonrosememorialfund.org'
+    h = line_height(pymupdf.Font(def_font), def_fontsize - 2)
+    insert_centered_text(ymax - h, text, fontsize=def_fontsize - 2)
 
     doc.save(output_path)
     doc.close()
@@ -142,7 +178,7 @@ def insert_form_fields(rows, page, ypos, xalign=False):
     def txtlen(s):
         return pymupdf.get_text_length(s, fontname=def_font, fontsize=def_fontsize)
 
-    largest_first_key = max([txtlen(row[0].get("label", "")) for row in rows])
+    largest_first_key = max([txtlen(row[0].get("label", "")) for row in rows if len(row) > 0])
 
     for row in rows:
         xpos = margin
@@ -152,12 +188,12 @@ def insert_form_fields(rows, page, ypos, xalign=False):
             label_len = txtlen(label)
             if i == 0 and xalign and txtlen(label) < largest_first_key:
                 label_len = largest_first_key
-            if label and field.get("name") and not (label[-1] in [",", ":", ";", "?", "!", "."]):
+            if label and field.get("name") and not reverse and not (label[-1] in [",", ":", ";", "?", "!", "."]):
                 label += ":"
                 label_len += 1
             if label and not reverse:
                 page.insert_text((xpos, ypos), label, fontname=def_font, fontsize=def_fontsize)
-                xpos += label_len + 4
+                xpos += label_len + 6
             if field.get("name"):
                 widget = pymupdf.Widget()
                 if field.get("size", -1) == -1:
@@ -177,6 +213,7 @@ def insert_form_fields(rows, page, ypos, xalign=False):
                 xpos += field["size"] + 4
             if label and reverse:
                 page.insert_text((xpos, ypos), label, fontname=def_font, fontsize=def_fontsize)
+                xpos += label_len + 6
 
             xpos += 20  # 20 horizontal padding in between fields
         ypos += 20
